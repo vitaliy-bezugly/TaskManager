d0bd31acf4325b96e4ac014b950b4171f1335593
Vitaliy <bezuglivita123@gmail.com>
2023-02-16 13:21:32 +0200

    Port by env variable

---
+++ Backend/TaskManager.Api/Helpers/SetterPortExtension.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskManager.Api.Helpers;
+
+public static class SetterPortExtension
+{
+    public static void ConfigurePortToListen(this WebApplicationBuilder builder)
+    {
+        string? port = Environment.GetEnvironmentVariable("PORT");
+        if (port != null)
+        {
+            builder.WebHost.UseUrls($"http://*:{port}");
+        }
+    }
+}
--- Backend/TaskManager.Api/Program.cs
+++ Backend/TaskManager.Api/Program.cs
@@ -1,10 +1,12 @@
 using TaskManager.Api;
+using TaskManager.Api.Helpers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var startup = new Startup(builder.Configuration);
-startup.ConfigureServices(builder.Services); // calling ConfigureServices method
+startup.ConfigureServices(builder.Services);            // calling ConfigureServices method
+builder.ConfigurePortToListen();
 
 var app = builder.Build();
 
-startup.Configure(app, builder.Environment); // calling Configure method
+startup.Configure(app, builder.Environment);            // calling Configure method
